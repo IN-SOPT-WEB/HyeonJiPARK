7ce984b964f0ba9e2800cc0fbadefb65eecc5a2d
parkhyeonji <iamphj3@naver.com>
2022-10-19 21:38:53 +0900

    init: 3주차 과제 폴더 세팅

--- week3/src/App.jsx
+++ week3/src/App.jsx
@@ -1,40 +1,15 @@
-// import React, { useState } from 'react';
-// import { useEffect } from 'react';
-// import Header from './components/Header';
-// import Nav from './components/Nav';
-import styled from 'styled-components'
-// import "./App.css";
+import React from 'react';
+import Header from './components/Header';
+import Content from './components/Content';
 
 function App() {
-//   const [count, setCount] = useState(0);
-
-// useEffect(() => {
-//   document.title=`${count} 세는 중`;
-
-//   return () => {
-//     document.title="원상복구";
-//   };
-
-// }, [count])
 
   return (
-    <div className="App">
-      {/* <Header data="1"/>
-      <Header data="2"/>
-      <Header data="3"/>
-      <Header data="4"/>
-      <Nav /> */}
-      {/* <button onClick={() => setCount(count+1)}>
-        count is {count}
-      </button> */}
-      <Title>Hello</Title>
+    <div>
+      <Header />
+      <Content />
     </div>
   );
 }
 
 export default App;
-
-const Title = styled.h1`
-  color: red;
-
-`;
