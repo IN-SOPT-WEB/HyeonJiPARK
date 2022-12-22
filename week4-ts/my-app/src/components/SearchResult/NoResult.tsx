46cea5325c64a1c71a5410ac60ad19d226f30c09
parkhyeonji <iamphj3@naver.com>
2022-12-23 00:34:07 +0900

    feat: 결과없음 화면 UI 구현

--- week4-ts/my-app/src/components/SearchResult/NoResult.tsx
+++ week4-ts/my-app/src/components/SearchResult/NoResult.tsx
@@ -1,12 +1,22 @@
-
 import React from 'react'
+import styled from "styled-components";
 
 function NoResult() {
   return (
-    <div>
-      결과없음
-    </div>
+    <NoResultContainer>
+      <p>앗! 없는 유저에요!</p>
+    </NoResultContainer>
   )
 }
 
 export default NoResult
+
+const NoResultContainer = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 200px 0px;
+
+  & > p {
+    margin: 0 auto;
+  }
+`
